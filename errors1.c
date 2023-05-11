e3a4f5456f196499a61c05aa338ddff2add91078
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-11 05:57:52 -0700

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -133,7 +133,8 @@ char *convert_number(long int num, int base, int flags)
 }
 
 /**
- * remove_comments - this is a function that removes or replaces first instance of '#' with '\0'
+ * remove_comments - this is a function that removes or
+ replaces first instance of '#' with '\0'
  * @buf: address of the string to remove or replace.
  *
  * Return: Absolutely always 0;
