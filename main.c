6c881eb6c1c952da64ac802b3b96aae3aec458d6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-12 15:11:35 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include "shell.h"
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - tests function that prints.
+ * @av: argument counts.
+ * @ac: argument vector.
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0 (Success)
  */
-int main(int ac, char **av)
+
+int main(int av, char **ac)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
@@ -17,18 +29,18 @@ int main(int ac, char **av)
 		: "=r" (fd)
 		: "r" (fd));
 
-	if (ac == 2)
+	if (av == 2)
 	{
-		fd = open(av[1], O_RDONLY);
+		fd = open(ac[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
+				_eputs(ac[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
+				_eputs(ac[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
@@ -39,6 +51,6 @@ int main(int ac, char **av)
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, av);
+	hsh(info, ac);
 	return (EXIT_SUCCESS);
 }
