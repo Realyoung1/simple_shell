792e4be162cb842d2dd67cb1d581a8c45a808645
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-16 12:30:31 -0700

    Update string.c

--- string.c
+++ string.c
@@ -1,26 +1,36 @@
 #include "shell.h"
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
+ * _strlen - wrote this functions returns the length of a strings.
+ * @s: the string whose length to check.
+ * Return: integer length of string.
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int v = 0;
 
 	if (!s)
 		return (0);
 	while (*s++)
-		i++;
-	return (i);
+		v++;
+	return (v);
 }
+
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
+ * _strcmp - functions that performs lexicogarphic comparison of two strangs
  * @s1: the first strang
  * @s2: the second strang
- *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
@@ -37,12 +47,12 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (*s1 < *s2 ? -1 : 1);
 }
+
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
- *
- * Return: address of next char of haystack or NULL
+ * starts_with - functions that check if needle starts with haystack.
+ * @haystack: strings to search.
+ * @needle: the substrings to find.
+ * Return: address of next char of haystack or NULL.
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -52,10 +62,9 @@ char *starts_with(const char *haystack, const char *needle)
 	return ((char *)haystack);
 }
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
- *
+ * _strcat - this functions concatenates two strings.
+ * @dest: the destination buffers.
+ * @src: the source buffers.
  * Return: pointer to destination buffer
  */
 char *_strcat(char *dest, char *src)
