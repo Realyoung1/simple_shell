43f95e9a6fcb2fa322465dece1990f2d534cf3ab
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-17 14:59:19 -0700

    Update main.c

--- main.c
+++ main.c
@@ -18,8 +18,7 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(int av, char **ac)
+int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
@@ -29,18 +28,18 @@ int main(int av, char **ac)
 		: "=r" (fd)
 		: "r" (fd));
 
-	if (av == 2)
+	if (ac == 2)
 	{
-		fd = open(ac[1], O_RDONLY);
+		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(ac[0]);
+				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(ac[1]);
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
@@ -51,6 +50,6 @@ int main(int av, char **ac)
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, ac);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
