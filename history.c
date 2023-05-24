250090cb426e0f7c453afc234fd526ddb089b2db
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-23 21:31:01 -0700

    Update history.c

--- history.c
+++ history.c
@@ -1,15 +1,4 @@
 #include "shell.h"
-#include <stdbool.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdlib.h>
 
 /**
  * get_history_file - this functions is to gets the history files.
