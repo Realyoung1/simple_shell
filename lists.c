bcebf230cad6937e8f1c9ebb3c200feb9aea4c88
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-23 22:08:10 -0700

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,4 +1,15 @@
 #include "shell.h"
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
  * add_node - the function that add a node to the start of the list.
