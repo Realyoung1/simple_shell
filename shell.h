242fbffa78ca159be9b75d33e225b5c8cfa70514
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-23 22:10:36 -0700

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,16 +1,17 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdlib.h>
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
