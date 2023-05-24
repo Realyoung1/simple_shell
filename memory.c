0d4155370122618d7690d493ecf5676774ba9cfe
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-05-23 21:34:03 -0700

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,15 +1,4 @@
 #include "shell.h"
-#include <stdbool.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdlib.h>
 
 /**
  * bfree - this is a function that frees a pointer and NULLs the addresses.
